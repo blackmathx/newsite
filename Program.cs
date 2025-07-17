637d8644da09bcd869ae75a2d1677859fbf68179
bo <jepayton12013@gmail.com>
2025-07-17 18:54:26 -0400

    update

--- Program.cs
+++ Program.cs
@@ -3,9 +3,32 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// This automatically loads appsettings.json + appsettings.Development.json (in Development)
+
+// handle connection for local vs prod
+var environment = builder.Environment;
+string? connectionString;
+
+
+if (environment.IsDevelopment())
+{
+	// Use connection string from appsettings.Development.json
+	connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+}
+else
+{
+	// Use environment variable in production
+	connectionString = Environment.GetEnvironmentVariable("DefaultConnection");
+
+	if (string.IsNullOrEmpty(connectionString))
+	{
+		throw new Exception("DefaultConnection environment variable not set.");
+	}
+}
+
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseNpgsql(connectionString));
+
+
 
 builder.Services.AddScoped<newsite.Services.PostRepository>();
 builder.Services.AddScoped<newsite.Services.UserCommentRepository>();
@@ -15,12 +38,14 @@ builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
+
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+	app.UseExceptionHandler("/Home/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -39,17 +64,6 @@ app.MapControllerRoute(
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
-
-using (var scope = app.Services.CreateScope())
-{
-	var services = scope.ServiceProvider;
-
-	var context = services.GetRequiredService<AppDbContext>();
-	Console.WriteLine("Applying migrations...");
-	context.Database.Migrate();
-	Console.WriteLine("Migrations applied successfully.");
-}
 
 
 app.Run();
